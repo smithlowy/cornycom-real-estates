f4b5bb0e58c0d57ab7316e001bf646184c0a4108
smithlowy <bajdroid350@gmail.com>
2026-04-13 17:50:19 +0300

    Configure media URL serving

--- uganda_land_app/urls.py
+++ uganda_land_app/urls.py
@@ -1,18 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
+from django.conf import settings  # Added this
+from django.conf.urls.static import static  # Added this
 
-# This function creates a simple message for the home page (the empty '')
 def home(request):
     return HttpResponse("Cornycom Real Estates API is Running! To see data, go to /api/plots/")
 
 urlpatterns = [
-    # 1. The Home Page
     path('', home), 
-
-    # 2. The Admin Panel
     path('admin/', admin.site.urls),
-
-    # 3. Your Listings API (CRITICAL: Keep this line!)
     path('api/', include('listings.urls')), 
 ]
+
+# --- ADD THIS AT THE VERY BOTTOM ---
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

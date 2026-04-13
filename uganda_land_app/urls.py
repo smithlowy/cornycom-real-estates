c2e606f58c3098de176f8a84400ca357669e4cc4
smithlowy <bajdroid350@gmail.com>
2026-04-13 15:28:22 +0300

    Added home page view and kept API paths

--- uganda_land_app/urls.py
+++ uganda_land_app/urls.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
-from django.urls import path, include # Make sure 'include' is here
+from django.urls import path, include
+from django.http import HttpResponse
+
+# This function creates a simple message for the home page (the empty '')
+def home(request):
+    return HttpResponse("Cornycom Real Estates API is Running! To see data, go to /api/plots/")
 
 urlpatterns = [
+    # 1. The Home Page
+    path('', home), 
+
+    # 2. The Admin Panel
     path('admin/', admin.site.urls),
-    # This connects everything together
+
+    # 3. Your Listings API (CRITICAL: Keep this line!)
     path('api/', include('listings.urls')), 
 ]
